f7d31e3abdee1249431efa967834058bf42b8006
John Stevenson <john@jr0cket.co.uk>
2018-09-15 23:33:08 +0100

    Added Hiccup and Bootstrap to create a better web page
    Created the basics of our monitor dashboard page without writing html direct.
    
    Added the Hiccup library to generate html from Clojure data structures and
    keywords.
    
    Using the hiccup.page/html5 function we created a page that allows us to include
    the Bootstrap CSS and JavaScript libraries.  Hiccup allows us to include CSS
    styles in the data structures, or more usefully refer to the Bootstrap styles by
    name.

--- src/status_monitor/handler.clj
+++ src/status_monitor/handler.clj
@@ -1,10 +1,44 @@
 (ns status-monitor.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [hiccup.core :refer :all]
+            [hiccup.page :refer :all]))
+
+(defn monitor-dashboard
+  "Returns a mock dashboard page"
+  [request]
+  (html5 {:ng-app "Mock Status Monitor Web App" :lang "en"}
+         [:head
+          [:title "Area51 Mock Status"]
+
+          ;; Bootstrap CSS and JavaScript libraries from the Content Delivery Network
+          (include-css "//stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css")
+          (include-js  "//stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js")
+          (include-js  "//stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.bundle.min.js")
+
+          [:body
+           ;; An invisible container to create a default margin at each side of the web page
+           [:div {:class "container"}
+
+            ;; Page Header using a large central banner, called a Jumbotron
+            [:div {:class "jumbotron"}
+             [:h1 "Mock Status Monitor Dashboard"]]
+
+            ;; Key systems to monitor displayed in a single row grid
+            [:div {:class "row"}
+             [:div {:class "col-md-4"}
+              [:h2 "Application monitor"]]
+
+             [:div {:class "col-md-4"}
+              [:h2 "Database monitor"]]
+
+             [:div {:class "col-md-4"}
+              [:h2 "Messaging monitor"]]]]]]))
+
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/" [] monitor-dashboard)
   (route/not-found "Not Found"))
 
 (def app
