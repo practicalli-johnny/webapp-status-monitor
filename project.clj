862284befd983b2cb47e96b80085a2fffd5078cb
John Stevenson <john@jr0cket.co.uk>
2018-09-15 22:25:58 +0100

    Updated project dependencies and plugin library versions
    The lein-compojure template is very simple to get started with, although it
    seems the libraries are a little behind the latest.  The project runs
    successfully without upgrading versions.  It is usually better to use the latest
    stable versions of these libraries to pick up any fixes.
    
    The latest stable versions were found via https://clojars.org/.
    
    Consider submitting a pull request to update the lein-compojure template project
    on Github.

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
 (defproject status-monitor "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A server side website dashboard to collate monitoring information"
+  :url "https://github.com/jr0cket/status-monitor"
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [compojure "1.5.1"]
-                 [ring/ring-defaults "0.2.1"]]
-  :plugins [[lein-ring "0.9.7"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [compojure "1.6.1"]
+                 [ring/ring-defaults "0.3.2"]]
+  :plugins [[lein-ring "0.12.4"]]
   :ring {:handler status-monitor.handler/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring/ring-mock "0.3.0"]]}})
+                        [ring/ring-mock "0.3.2"]]}})
